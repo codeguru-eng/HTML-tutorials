6eb3fd399de6a98cda2217a2e6388bf6db3e9a44
codeguru-eng <81927198+codeguru-eng@users.noreply.github.com>
2021-04-12 21:32:37 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,48 @@
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+
+function myFunction() {
+  var input = document.getElementById("Search");
+  var filter = input.value.toLowerCase();
+  var nodes = document.getElementsByClassName('target');
+
+  for (i = 0; i < nodes.length; i++) {
+    if (nodes[i].innerText.toLowerCase().includes(filter)) {
+      nodes[i].style.display = "block";
+    } else {
+      nodes[i].style.display = "none";
+    }
+  }
+}
+
+
+function onFunction() {
+	alert("You are back online");
+	document.getElementById('body').style.display = 'block';
+}
+
+function offFunction() {
+	alert("No internet, You are offline. Try reloading the page or check your internet connection.");
+	document.getElementById('body').style.display = 'none';
+	
+
+}
+
+
